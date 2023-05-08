56a5edac0830d680aab5ad4de59a98ec1ea8be0b
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-08 06:01:28 -0700

    10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
- * main - check the code
- *
+ * check_cycle - check the code
+ * @list: list
  * Return: Always 0.
  */
 int check_cycle(listint_t *list)
@@ -10,9 +10,9 @@ if (list == NULL)
 return (0);
 else
 {
-if (list->next == NULL)
-return (0);
-else
+if (list->next)
 return (1);
+else
+return (0);
 }
 }
