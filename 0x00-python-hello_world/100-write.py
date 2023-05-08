cd5b4946e8509685157a7f51d69fcbc0ba59aadd
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-08 05:41:59 -0700

    100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.write(1, 'and that piece of art is useful - Dora Korpar, 2015-10-19\n',58)
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 sys.exit(1)
