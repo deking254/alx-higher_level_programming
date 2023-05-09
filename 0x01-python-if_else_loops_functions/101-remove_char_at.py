7eefcde77da203f3167160b2ae6329ba3f7e0182
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-09 12:20:48 -0700

    101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,11 @@
-d
+#!/usr/bin/python3
+def remove_char_at(st, n):
+    i = 0
+    string = ""
+    for s in st:
+        if i == n:
+            i += 1
+        else:
+            string += s
+        i += 1
+    return string
