e51e16f96768bfe2e2cdbd094bd889382db565be
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-09 06:42:30 -0700

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
     if number != 99:
-        print(f"{number}", end=", ")
+        print("{}".format(number), end=", ")
     else:
-        print(f"{number}", end="\n")
+        print("{}".format(number), end="\n")
