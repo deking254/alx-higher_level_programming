c72b22d1e44a35f9015ba8764e6fced0e3ccad3c
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-09 08:41:38 -0700

    100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,6 @@
-d
+#!/usr/bin/python3
+for letter in range(122, 96, -1):
+    if letter % 2 == 0:
+        print("{}".format(chr(letter)), end="")
+    if letter % 2 != 0:
+        print("{}".format(chr(letter - 32)), end="")
