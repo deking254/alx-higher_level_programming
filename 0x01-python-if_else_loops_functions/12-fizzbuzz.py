aa3f07cc498e33846e2a24034a631889106ed661
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-09 08:04:55 -0700

    12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for n in range(1, 101):
-        if n % 3 == 0 and n % 5 != 0:
-            print("Fizz", end=", ")
-        if n % 5 == 0 and n % 3 != 0 and n != 100:
-            print("Buzz", end=", ")
-        if n % 3 == 0 and n % 5 == 0:
-            print("FizzBuzz", end=", ")
+        if n % 3 == 0:
+            if n % 5 == 0:
+                print("FizzBuzz", end=", ")
+            else:
+                print("Fizz", end=", ")
+        if n % 5 == 0 and n != 100:
+            if n % 3 == 0:
+                print("FizzBuzz", end=", ")
+            else:
+                print("Buzz", end=", ")
+        if n % 3 != 0 or n % 5 != 0 or n != 100:
+            print("{}".format(n), end=", ")
         if n == 100:
-            print("Buzz", end=" ")
-        else:
-            print(f"{n}", end=", ")
+            print("{}".format("Buzz"), end=" ")
 
