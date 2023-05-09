5ee9bf7c557ca420c7523aeebaa177c3b1e26da5
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-09 06:19:07 -0700

    1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,9 +10,10 @@ if number >= 0:
     if int(numberstr[-1]) == 0:
         print(f"Last digit of {number} is {numberstr[-1]} and is 0")
 if number < 0:
+    nega = int(numberstr[-1])* -1;
     if int(numberstr[-1])* -1 > 5:
-        print(f"Last digit of {number} is {numberstr[-1]} and is greater than 5")
+        print(f"Last digit of {number} is {nega} and is greater than 5")
     if int(numberstr[-1])* -1 < 6 and int(numberstr[-1])* -1 != 0:
-        print(f"Last digit of {number} is {numberstr[-1]} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {nega} and is less than 6 and not 0")
     if int(numberstr[-1]) == 0:
         print(f"Last digit of {number} is {numberstr[-1]} and is 0")
