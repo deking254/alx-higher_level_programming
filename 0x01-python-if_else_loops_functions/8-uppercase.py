727bb598a65152c398d57916dce1dcbd170a6d34
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-09 07:22:55 -0700

    8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for letter in str:
-        print(f"{ord(letter)- 32:c}" if ord(letter)>=97 and ord(letter)<=122 else f"{letter}", end="")
-    print('',end="\n")
+    for l in str:
+        if ord(l) >= 97 and ord(l) <= 122:
+            lower = 1
+        if ord(l) >= 65 and ord(l) <= 90:
+            lower = 0
+        if ord(l) == 32:
+            lower = 0
+        print("{}".format(chr(ord(l) - 32) if lower else l), end="")
+    print("{}".format(''), end="\n")
