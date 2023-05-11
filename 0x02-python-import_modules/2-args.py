039231a37f62d002ca3a22e585e55038ee8e1b5e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-11 02:21:18 -0700

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,9 +7,9 @@ if __name__ == '__main__':
         print("{} arguments.".format(arguments))
     if arguments == 1:
         print("{} argument:".format(arguments))
-        print("{} {}".format(1, sys.argv[1]))
+        print("{}: {}".format(1, sys.argv[1]))
     if arguments > 1:
         print("{} arguments:".format(arguments))
         for argument in sys.argv[1:]:
-            print("{} {}".format(i, argument))
+            print("{}: {}".format(i, argument))
             i += 1
