2d7b0388749c02edca3744bcebbcc039e883bbbd
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-13 12:50:40 -0700

    13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,1 +1,42 @@
-d
+#include "lists.h"
+#include <stddef.h>
+/**
+ * is_palindrome - check the code for
+ * @head: rt
+ * Return: Always 0.
+ */
+int is_palindrome(listint_t **head)
+{
+int j, i = 0, h = 0, k = 0;
+listint_t *hd = *head;
+listint_t *hed = *head;
+listint_t *heed = *head;
+while (hd->next)
+{
+hd = hd->next;
+i++;
+}
+i--;
+if (i % 2 != 0)
+{
+j = (i / 2) + 1;
+}
+else
+j = i / 2;
+while (h <= j)
+{
+while (k <= i)
+{
+hed = hed->next;
+k++;
+}
+if (heed->n != hed->n)
+return (0);
+h++;
+heed = heed->next;
+i--;
+k = 0;
+hed = *head;
+}
+return (1);
+}
