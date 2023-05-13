9124e0afb31208ec239603a69d63407bdadfc39d
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-13 01:18:08 -0700

    3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     length = -len(my_list)
     for liist in range(-1, length - 1, -1):
-        print(my_list[liist])
+        print("{:d}".format(my_list[liist]))
