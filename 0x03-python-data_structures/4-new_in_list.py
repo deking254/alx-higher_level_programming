dbca8154d4e1f010bf34d5e3d7c40712cbec6c59
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-13 02:14:55 -0700

    4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,11 +3,11 @@ def new_in_list(my_list, idx, element):
     length = len(my_list)
     i = 0
     newlist = [None] * length
-    if idx < 0 or idx > length - 1:
+    if idx > length - 1:
         return (my_list)
     else:
         for listt in my_list:
-            if i != element:
+            if i != idx:
                 newlist[i] = listt
             else:
                 newlist[i] = element
