5ef2250111e53b1a930c0d7a96d44e5ea990e42f
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-13 08:48:50 -0700

    100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,4 +1,5 @@
-#include <object.h>
+#include <Python.h>
+#include <stdio.h>
 /**
  * print_python_list_info - check the code
  * @p: object
@@ -6,5 +7,12 @@
  */
 void print_python_list_info(PyObject *p)
 {
-printf(p[0])
+int i;
+PyListObject *list = (PyListObject *)p;
+printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+printf("[*] Allocated = %ld\n", list->allocated);
+for (i = 0; i < PyList_Size(p); i++)
+{
+printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+}
 }
