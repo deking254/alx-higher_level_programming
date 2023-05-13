f018105c201f862f182fccc5eef037be34979803
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-13 05:11:00 -0700

    11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if my_list != None:
+    if my_list is not None:
         length = len(my_list)
         if idx < length and idx >= 0:
             del(my_list[idx])
