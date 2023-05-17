86103e5a08590991e8cd5552799d5be917e422fb
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-17 03:54:20 -0700

    8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    keys =  list(a_dictionary.keys())
+    keys = list(a_dictionary.keys())
     for ke in keys:
         if ke == key:
             a_dictionary.pop(key)
