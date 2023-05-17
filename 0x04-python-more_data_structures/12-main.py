d74ebe5e7a76ca3ab5b56a466c11534943732d7c
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-17 06:55:41 -0700

    12

--- 0x04-python-more_data_structures/12-main.py
+++ 0x04-python-more_data_structures/12-main.py
@@ -15,5 +15,5 @@ print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 roman_number = "LXXXVII"
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
 
-roman_number = "DCCVII"
+roman_number = 50
 print("{} = {}".format(roman_number, roman_to_int(roman_number)))
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,24 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 def roman_to_int(roman_string):
-    return (0)
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    total = 0
+    if not isinstance(roman_string, str):
+        return (0)
+    else:
+        arr = [None] * len(roman_string)
+        if len(roman_string) > 1:
+            if romans.get(roman_string[0]) < romans.get(roman_string[1]):
+                arr[0] = romans.get(roman_string[0]) * -1
+                for i in range(1, len(roman_string)):
+                    arr[i] = romans.get(roman_string[i])
+                for num in arr:
+                    total += num
+                return (total)
+            else:
+                for i in range(0, len(roman_string)):
+                    arr[i] = romans.get(roman_string[i])
+                for num in arr:
+                    total += num
+                return (total)
+        if len(roman_string) == 1:
+            return (romans.get(roman_string[0]))
