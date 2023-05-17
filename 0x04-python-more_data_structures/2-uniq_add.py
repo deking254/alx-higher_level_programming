39dec8e312226866daffa477c9caff41a39441a4
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-17 03:47:25 -0700

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,7 +3,7 @@ def uniq_add(my_list=[]):
     summation = 0
     for my in my_list:
         summation += my
-        for i in range(0,len(my_list)):
+        for i in range(0, len(my_list)):
             if my == my_list[i]:
                 my_list[i] = 0
     return (summation)
