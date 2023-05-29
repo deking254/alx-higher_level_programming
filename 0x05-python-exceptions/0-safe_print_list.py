1bcd50c639603197394e0ba2b5b8527adeba0b4a
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-29 06:56:07 -0700

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     i = 0
-    j = 0
-    for listi in my_list:
-        j += 1
     for number in range(0, x):
         try:
-            if number == x - 1 or number == j - 1:
-                print(my_list[number])
-                i += 1
-                return (i)
-            else:
-                print(my_list[number], end="")
-                i += 1
+            print(my_list[number], end="")
+            i += 1
         except IndexError:
             break
+    print("")
     return (i)
