19acdb9d40d3bc7c7036442a73ad749a1908a061
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-29 09:06:53 -0700

    a

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,5 +5,5 @@ def safe_function(fct, *args):
         result = fct(args[0], args[1])
         return (result)
     except Exception as e:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        print("Exception: {}".format(e))
         return (None)
