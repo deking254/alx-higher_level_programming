b0485f1e8593fb941705984abcef8ac61d538283
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-29 06:51:47 -0700

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -13,6 +13,6 @@ def safe_print_list(my_list=[], x=0):
             else:
                 print(my_list[number], end="")
                 i += 1
-        except:
-            continue
+        except IndexError:
+            break
     return (i)
