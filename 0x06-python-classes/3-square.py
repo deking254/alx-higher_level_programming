384d1f8809a17e8fa11b2a5feafbb6f6f1ef647b
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-30 04:02:57 -0700

    commit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-def area(self):
-    return (self ** 2)
+"""This is a module for greeting and performing square calculations."""
+
+
 class Square:
+    """square class"""
     __size = None
+
     def __init__(self, size=0):
+        """init func"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
+        """area func"""
         return (self.__size ** 2)
