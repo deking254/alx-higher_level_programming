1fa3444e1a46205744cc3470bb34806ce664e6ff
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-30 06:58:54 -0700

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,7 +40,7 @@ class Square:
     @position.setter
     def position(self, value):
         """position setter"""
-        info = "position musti be a tuple of 2 positive integers"
+        info = "position must be a tuple of 2 positive integers"
         if type(value) is tuple and len(value) == 2:
             if type(value[0]) is int and type(value[1]) is int:
                 if value[0] >= 0 and value[1] >= 0:
@@ -59,7 +59,7 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        for l in range(self.__position[1]):
+        for _ in range(self.__position[1]):
             print("")
         for _ in range(self.__size):
             print(" " * self.__position[0], end="")
