2de61964883c864d143ffec7ae725c0dcc3b67db
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-30 03:55:45 -0700

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""This is a module for greeting and performing square calculations."""
+
+
 class Square:
+    """class for square"""
     __size = None
+
     def __init__(self, size=3):
+        """init func"""
         self.__size = size
