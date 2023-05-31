1dc704dc219bb9e42d5ce2a76a9e4965689a7b6c
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-05-31 00:29:14 -0700

    101

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,1 +1,15 @@
-d
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result += a + b
+            break
+    return result
+import dis
+dis.dis(magic_calculation)
