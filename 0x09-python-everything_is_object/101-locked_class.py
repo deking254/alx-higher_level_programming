e9519e86e8b2d24ae1f8b9e71da8f925d1612d9d
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-06 08:06:50 -0700

    101

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,9 +2,12 @@
 """
     the class of locked
 """
+
+
 class LockedClass:
     """the class itself"""
     __slots__ = ["first_name"]
+
     def __init__(self):
         """the init func"""
         self.first_name = "first_name"
