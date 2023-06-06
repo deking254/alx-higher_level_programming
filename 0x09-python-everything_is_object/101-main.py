9b387c3164c7dddec66ecb73e79473d2fec3921d
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-06 07:12:55 -0700

    101

--- 0x09-python-everything_is_object/101-main.py
+++ 0x09-python-everything_is_object/101-main.py
@@ -2,13 +2,10 @@
 LockedClass = __import__('101-locked_class').LockedClass
 
 lc = LockedClass()
+print("----")
+print(lc)
 lc.first_name = "John"
-print(lc.first_name)
 try:
-    print(lc.last_name)
     lc.last_name = "Snow"
-    print(lc.last_name)
-    lc.gere = "ty"
-    print(lc.gere)
 except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
