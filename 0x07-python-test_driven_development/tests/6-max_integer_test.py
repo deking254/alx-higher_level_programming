5a220a8530599f358ad14e71a7f76adbaa450d31
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-07 02:01:04 -0700

    0

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -14,3 +14,15 @@ class TestMaxInteger(unittest.TestCase):
     def test_last(self):
         result = max_integer([2, 5, 7, -1, 9, 20])
         self.assertEqual(result, 20)
+    def test_first(self):
+        result = max_integer([20, 5, 7, -1, 9, 10])
+        self.assertEqual(result, 20)
+    def test_allneg(self):
+        result = max_integer([-2, -5, -7, -1, -9, -20])
+        self.assertEqual(result, -1)
+    def test_single(self):
+        result = max_integer([20])
+        self.assertEqual(result, 20)
+    def test_empty(self):
+        result = max_integer([])
+        self.assertEqual(result, None)
