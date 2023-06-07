52ebca851d1148c7f8c3dd098e9f33fa79eccb49
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-07 03:44:23 -0700

    0

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-    a function that multiplies 
-    2 matrices by using 
-    the module NumPy
-"""
+"""a function that multiplies 2 matrices by using the module NumPy"""
 import numpy as np
 
+
 def lazy_matrix_mul(m_a, m_b):
-    """
-        a function that multiplies 
-        2 matrices by using 
-        the module NumPy
-    """
+    """a function that multiplies 2 matrices by using the module NumPy"""
     arr_one = np.array(m_a)
     arr_two = np.array(m_b)
     try:
