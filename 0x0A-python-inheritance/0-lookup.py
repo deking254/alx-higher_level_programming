45181c901f30c806e29f7832f720538cce291d97
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-12 06:05:34 -0700

    1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""this  function returs the list of available attributes and methods of an object"""
+"""this  function returs the list of available
+attributes and methods of an object
+"""
+
+
 def lookup(obj):
-    """this  function returs the list of available attributes and methods of an object"""
-    return (dir(obj));
+    """this  function returs the list of available
+    attributes and methods of an object
+    """
+    return (dir(obj))
