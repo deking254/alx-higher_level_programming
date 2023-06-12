0cad93a960b628837cfe2f200288c0e98e5bc0da
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-12 06:16:50 -0700

    7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
     def area(self):
         """raises an Exception with the message area() is not implemented"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """method that validates value"""
         if type(value) != int:
