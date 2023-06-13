72f2497bef21bda7a303dc0da6e45921f0254733
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-13 09:36:51 -0700

    m

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,17 @@
-d
+#!/usr/bin/python3
+"""this module seeks to create pascals trangle"""
+
+
+def pascal_triangle(n):
+    """function that returns a list of lists of integers"""
+    outer = []
+    for row in range(n):
+        inner = []
+        for col in range(row + 1):
+            if col == 0 or col == row:
+                inner.append(1)
+            else:
+                temp_inner = outer[row - 1]
+                inner.append(temp_inner[col - 1] + temp_inner[col])
+        outer.append(inner)
+    return(outer)
