b03129894802cf1e1d167a9a27fd14499823af5e
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-13 05:45:47 -0700

    m

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,14 @@ import json
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-lis = []
-for i in range(1, len(sys.argv)):
-    lis.append(sys.argv[i])
-save_to_json_file(lis, "add_item.json")
-load_from_json_file("add_item.json")
+try:
+    lis = load_from_json_file("add_item.json")
+    for i in range(1, len(sys.argv)):
+        lis.append(sys.argv[i])
+    save_to_json_file(lis, "add_item.json")
+except Exception:
+    lis = []
+    for i in range(1, len(sys.argv)):
+        lis.append(sys.argv[i])
+    save_to_json_file(lis, "add_item.json")
+    load_from_json_file("add_item.json")
