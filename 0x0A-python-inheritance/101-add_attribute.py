b2d622384c5a0b75b2a12d7f77fe76bd35d9213b
DENNIS KIPTOO NGETICH <dekingsky@gmail.com>
2023-06-14 05:55:02 -0700

    m

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,7 @@
-d
+#!/usr/bin/python3
+"""module to add attr to object"""
+
+
+def add_attribute(obj, attr="", val=""):
+    """a function that adds a new attribute to an objec"""
+    obj.__setattr__(attr, val)
