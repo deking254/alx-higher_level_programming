c64e2ff8091e69b26ccd5cacb458d6d7fc31d4f2
deking254 <dekingsky@gmail.com>
2023-07-19 16:49:07 +0000

    q

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,1 +1,2 @@
-d
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT `title`, SUM(`rate`) AS `r` FROM `tv_shows` AS s INNER JOIN `tv_show_ratings` AS rat ON s.`id` = rat.`show_id` GROUP BY `title` ORDER BY `r` DESC;
