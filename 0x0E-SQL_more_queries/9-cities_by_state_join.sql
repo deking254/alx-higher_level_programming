43187c28257f60e0963caa957669be3801c877a4
deking254 <dekingsky@gmail.com>
2023-07-19 19:07:47 +0000

    9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database
-SELECT cities.id,cities.name,states.name FROM cities, states ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities,states WHERE cities.state_id = states.id ORDER BY cities.id ASC;
