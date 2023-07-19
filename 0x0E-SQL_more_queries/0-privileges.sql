2ca323298c8528b6dfbced8b8667a111627aa926
deking254 <dekingsky@gmail.com>
2023-07-19 15:53:51 +0000

    0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,10 +1,3 @@
 -- list the privileges of users user_0d_1 and user_02
-
-SELECT
-    CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS show_grants_query
-FROM
-    mysql.user
-WHERE
-    user IN ('user_0d_1', 'user_0d_2')
-    AND host = 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
