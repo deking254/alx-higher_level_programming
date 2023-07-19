fb521931a356bc3c79d8d543c787b4dce16b56cf
deking254 <dekingsky@gmail.com>
2023-07-19 15:48:18 +0000

    0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- list the privileges of users user_0d_1 and user_02
-CREATE USER 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
