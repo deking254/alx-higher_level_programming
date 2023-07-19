d7105fb5d4f58f6efc92f5c916ba41ad041bb35f
deking254 <dekingsky@gmail.com>
2023-07-19 16:28:05 +0000

    1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,9 @@
 -- creates a user and gives all privilleges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY mysql_native_password 'user_0d_1_pwd';
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+	    IDENTIFIED BY 'user_0d_1_pwd';
+	GRANT ALL PRIVILEGES
+	   ON *.*
+	   TO 'user_0d_1'@'localhost'
+	   IDENTIFIED BY 'user_0d_1_pwd';
+	FLUSH PRIVILEGES;
