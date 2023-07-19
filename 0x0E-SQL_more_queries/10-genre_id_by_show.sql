6365e844d8a4a78b51438997ec3f3e67c744fb6e
deking254 <dekingsky@gmail.com>
2023-07-19 19:25:47 +0000

    10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
--- Import the database dump from hbtn_0d_tvshows
-SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- Import the database dump from hbtn_0d_tvshowi
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres WHERE tv_show_genres.show_id = tv_shows.id  ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
