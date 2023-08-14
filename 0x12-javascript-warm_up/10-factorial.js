d13c6bf40479a8c8c469bcf29288c3f59e066075
deking254 <dekingsky@gmail.com>
2023-08-14 15:54:45 +0000

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-let a = 1;
-let c = 1;
-if (process.argv[2]) {
-  const b = parseInt(process.argv[2]);
-  if (b) {
-    while (c <= b) {
-      a = a * c;
-      c++;
-    }
-    console.log(a);
+function factorio (b) {
+  let a = 1;
+  let c = 1;
+  while (c <= b) {
+    a = a * c;
+    c++;
   }
+  console.log(a);
+}
+if (process.argv[2]) {
+  const e = parseInt(process.argv[2]);
+  if (e) { factorio(e); }
 } else { console.log(a); }
