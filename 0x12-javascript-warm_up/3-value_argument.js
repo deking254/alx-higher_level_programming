75b5aabadff9b55383fb8db38d259f18e4dfeb47
deking254 <dekingsky@gmail.com>
2023-08-14 15:26:36 +0000

    3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 let i = 0;
-while (process.argv[i]) {
-  if (i === 2) {
-    console.log(process.argv[i]);
-  }
-  i += 1;
+while (process.argv[i])
+{
+if (i === 2)
+{
+console.log(process.argv[i]);
+}
+i += 1;
 }
 if (i === 2)
-console.log("No argument");
+{
+console.log('No argument');
+}
