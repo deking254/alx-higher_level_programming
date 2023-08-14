fc2e8723de7cb480130ee12bb4e222a4addd83c8
deking254 <dekingsky@gmail.com>
2023-08-14 15:45:48 +0000

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 let a = 1;
 let c = 1;
-let b = parseInt(process.argv[2]);
-if (b) {
-  while (c <= b) {
-    a = a * c;
-    c++;
+if (process.argv[2]) {
+  const b = parseInt(process.argv[2]);
+  if (b) {
+    while (c <= b) {
+      a = a * c;
+      c++;
+    }
+    console.log(a);
   }
-  console.log(a);
 } else { console.log(a); }
