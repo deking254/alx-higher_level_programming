6e880968ec4e9db682037eac024393baad412991
deking254 <dekingsky@gmail.com>
2023-08-15 15:06:17 +0000

    101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-const dct = require('101-data').dict;
+const dct = require('./101-data').dict;
+const newish = {};
+let key = '';
+for (key in dct) {
+  newish[dct[key]] = [];
+}
+for (key in dct) {
+  newish[dct[key]].push(key);
+}
+console.log(newish);
