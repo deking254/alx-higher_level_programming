29cbf6aa6d009c10f663842f61fef91b44327b55
deking254 <dekingsky@gmail.com>
2023-08-16 11:28:21 +0000

    102

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,14 @@
-d;
+#!/usr/bin/node
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) { return; }
+  fs.writeFile(process.argv[4], data, 'utf8', (err) => {
+    if (err) { console.log('Error'); }
+  });
+});
+fs.readFile(process.argv[3], 'utf8', (err, dat) => {
+  if (err) { return; }
+  fs.appendFile(process.argv[4], dat, 'utf8', (err) => {
+    if (err) { console.log('Error'); }
+  });
+});
