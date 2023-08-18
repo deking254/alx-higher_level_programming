1f9cf9a5e303edf11bf9c178b0d394f94567c882
deking254 <dekingsky@gmail.com>
2023-08-18 12:24:09 +0000

    2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import MySQLdb;
-import sys;
+"""script that takes in an argument and displays all values"""
+import MySQLdb
+import sys
 
 
 if __name__ == '__main__':
-    """the main entry"""
-    arguments = sys.argv;
-    db = MySQLdb.Connection("localhost", arguments[1], arguments[2], arguments[3]);
-    cur = db.cursor();
-    ty = cur.execute("SELECT * FROM states WHERE states.name='{}' ORDER BY states.id ASC".format(arguments[4]));
-    lst =cur.fetchall();
+    arg = sys.argv
+    db = MySQLdb.Connection("localhost", arg[1], arg[2], arg[3])
+    cur = db.cursor()
+    quer = "SELECT * FROM states WHERE states.name='{}' ORDER BY states.id ASC"
+    ty = cur.execute(quer.format(arg[4]))
+    lst = cur.fetchall()
     for row in lst:
-        print(row);
+        print(row)
