5928808f67a850316c171dcd9b264d8be7e18957
deking254 <dekingsky@gmail.com>
2023-09-01 14:48:45 +0000

    3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""displays yhe body of the response and handles error"""
+from urllib import request
+import urllib
+import sys
+
+if __name__ == "__main__":
+    try:
+        with request.urlopen(sys.argv[1]) as res:
+            print(res)
+    except (urllib.error.HTTPError) as e:
+        print("Error code: " + str(e.getcode()))
