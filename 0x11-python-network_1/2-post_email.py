9f6377eb209bb8d3666b2e1c1197d1617272470b
deking254 <dekingsky@gmail.com>
2023-09-01 14:09:33 +0000

    2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,6 @@ import sys
 
 if __name__ == "__main__":
     new = parse.urlencode({"email": sys.argv[2]})
-    print(new)
     with request.urlopen(sys.argv[1], new.encode('ascii')) as req:
         res = req.read()
         print(res.decode('utf-8'))
