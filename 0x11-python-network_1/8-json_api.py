654480a8d8281921f7f10e7059deefca072dfbcd
deking254 <dekingsky@gmail.com>
2023-09-01 16:06:58 +0000

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""the search Api usingthe requaests package"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        payload = dict(q=sys.argv[1])
+    else:
+        payload = dict(q="")
+    with requests.post('http://0.0.0.0:5000/search_user', payload) as res:
+        j = res.json()
+        if j:
+            print('[' + str(j.get('id')) + '] ' + j.get('name'))
+        else:
+            print('No result')
