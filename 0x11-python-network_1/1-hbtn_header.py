b518cad09dc5342e38645658d107b984800378d8
deking254 <dekingsky@gmail.com>
2023-09-01 11:04:07 +0000

    1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""get the header X-Request-Id"""
+from urllib import request
+import sys
+
+if __name__ == "__main__":
+    with request.urlopen(sys.argv[1]) as req:
+        print(req.getheader('X-Request-Id'))
