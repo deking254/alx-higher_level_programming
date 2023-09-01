07459c8928206e35a1685ce02fbc45d8a4a8c0df
deking254 <dekingsky@gmail.com>
2023-09-01 15:14:04 +0000

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""displays the value of the variable X-Request-Id"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    with requests.get(sys.argv[1]) as res:
+        print(res.headers.get('X-Request-Id'))
