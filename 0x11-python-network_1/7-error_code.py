f3391888f77477e2421817813951d2baac285409
deking254 <dekingsky@gmail.com>
2023-09-01 15:36:56 +0000

    7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""detects the error returned by response"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    with requests.get(sys.argv[1]) as res:
+        if res.status_code == 200:
+            print(res.text)
+        else:
+            print('Error code: ' + str(res.status_code))
