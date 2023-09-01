81aefb41b695dd19b3ad210b14441ab705de8055
deking254 <dekingsky@gmail.com>
2023-09-01 15:02:24 +0000

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""fetches a url using the request package"""
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: ' + str(r.text.__class__))
+    print('\t- content: ' + r.text)
