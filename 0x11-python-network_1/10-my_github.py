1aba8e4fbebb47533dd9afbac7e08dc57ecf4c79
deking254 <dekingsky@gmail.com>
2023-09-01 19:51:10 +0000

    10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""the hithub Api used to retrieve users"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    payload = dict(Authorization="Bearer " + sys.argv[2])
+    url = 'https://api.github.com/users/'
+    with requests.get(url + sys.argv[1], payload) as res:
+        try:
+            j = res.json()
+            if j:
+                print(j.get('id'))
+            else:
+                print(None)
+        except Exception:
+            pass
