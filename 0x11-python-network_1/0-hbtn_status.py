7833d6b5a0e77de25b050d1fc64b12138bad8a4d
deking254 <dekingsky@gmail.com>
2023-09-01 11:23:07 +0000

    all

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         r = res.read()
         print('Body response:')
-        print('    - type: ' + str(r.__class__))
-        print('    - content: ' + str(r))
-        print('    - utf8 content: ' + r.decode('utf-8'))
+        print('\t- type: ' + str(r.__class__))
+        print('\t- content: ' + str(r))
+        print('\t- utf8 content: ' + r.decode('utf-8'))
