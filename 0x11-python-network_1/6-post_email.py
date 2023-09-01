ca952545b49e22bbea5a1fbbb5fedb6726de1349
deking254 <dekingsky@gmail.com>
2023-09-01 15:21:42 +0000

    6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""sends a POST request to the passed URL with the email"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    payload = dict(email=sys.argv[2])
+    with requests.post(sys.argv[1], payload) as res:
+        print(res.text)
