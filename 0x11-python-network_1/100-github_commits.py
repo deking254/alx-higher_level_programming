ba85b54a3ee0d438d7781401123c825429d9cd56
deking254 <dekingsky@gmail.com>
2023-09-02 10:39:32 +0000

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""get the list of candidates"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    a = sys.argv[1]
+    b = sys.argv[2]
+    token = "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"
+    autho = dict(Authorization="Bearer " + token)
+    url = 'https://api.github.com/repos/' + b + "/" + a + "/" + "commits"
+    with requests.get(url) as res:
+        js = res.json()
+        i = 0
+        for j in js:
+            name = j.get('commit').get('author').get('name')
+            if i < 10:
+                print("{}: {}".format(j.get("sha"), name))
+                i = i + 1
+            else:
+                break
