41878171a7b3621b0c7da866497374ccacc82379
deking254 <dekingsky@gmail.com>
2023-09-26 11:39:30 +0000

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const req = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+req.get(url, (error, response) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const res = JSON.parse(response.body);
+    console.log(res.title);
+  }
+});
