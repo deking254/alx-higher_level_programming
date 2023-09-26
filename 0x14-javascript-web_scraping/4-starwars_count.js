0541f97bee0305999a0ce147e8606e75eb24e167
deking254 <dekingsky@gmail.com>
2023-09-26 14:43:50 +0000

    14

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,17 @@
 const req = require('request');
 const id = process.argv[2];
 let k = 0;
-let i, films, j;
+let i, films, j, res;
 const person = 'https://swapi-api.alx-tools.com/api/people/18/';
 req.get(id, (error, response) => {
   if (error) {
     console.log(error);
   } else {
-    const res = JSON.parse(response.body);
+    try {
+      res = JSON.parse(response.body);
+    } catch (error) {
+      return;
+    }
     films = res.results;
     for (i in films) {
       const film = films[i];
