73db73d2c3a0b711c58468d18cb8830528c3213c
deking254 <dekingsky@gmail.com>
2023-09-26 15:22:01 +0000

    5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const req = require('request');
+const fs = require('fs');
+const args = process.argv;
+const url = args[2];
+let res;
+const Filename = args[3];
+req.get(url, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    res = data.body;
+    fs.writeFile(Filename, res, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
+});
