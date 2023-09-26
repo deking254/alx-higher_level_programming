10f9647c88705eb52605416f3a15165ed9bd4832
deking254 <dekingsky@gmail.com>
2023-09-26 16:04:30 +0000

    6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const req = require('request');
+const url = process.argv[2];
+let todos, i;
+req.get(url, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    todos = JSON.parse(data.body);
+    const n = {};
+    for (i in todos) {
+      if (todos[i].completed) {
+        const property = String(todos[i].userId);
+        if (n[property]) {
+          n[property] = n[property] + 1;
+        } else {
+          n[property] = 1;
+        }
+      }
+    }
+    console.log(n);
+  }
+});
