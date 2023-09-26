2807ec7782a917cee7467c91b7c43952b1f77029
deking254 <dekingsky@gmail.com>
2023-09-26 10:23:20 +0000

    1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const fs = require('fs');
+const args = process.argv;
+const Filename = args[2];
+const data = args[3];
+fs.writeFile(Filename, data, 'utf8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
