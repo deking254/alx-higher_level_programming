c9e90d3dfd6c7c8991d88283a1572fc475f0568c
deking254 <dekingsky@gmail.com>
2023-09-26 16:21:56 +0000

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+const req = require('request');
+const id = process.argv[2];
+let characters, i, person;
+const movies = 'https://swapi-api.alx-tools.com/api/films/' + id + '/';
+req.get(movies, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    characters = JSON.parse(data.body).characters;
+    for (i in characters) {
+      req.get(characters[i], (error, data) => {
+        if (error) {
+          console.log(error);
+        } else {
+          person = JSON.parse(data.body).name;
+          console.log(person);
+        }
+      });
+    }
+  }
+});
