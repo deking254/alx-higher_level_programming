51479f423303960217691c4933447c4363dc86a6
deking254 <dekingsky@gmail.com>
2023-09-27 08:00:04 +0000

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,7 +1,18 @@
 #!/usr/bin/node
 const req = require('request');
+const util = require('util');
+const requestPromise = util.promisify(req);
 const id = process.argv[2];
-let characters, i, person;
+let characters, i;
+
+async function makeGetRequest (url) {
+  const response = await requestPromise(url);
+  if (response.statusCode !== 200) {
+    throw new Error(`HTTP Status Code: ${response.statusCode}`);
+  }
+  const responseBody = response.body;
+  return responseBody;
+}
 const movies = 'https://swapi-api.alx-tools.com/api/films/' + id + '/';
 req.get(movies, (error, data) => {
   if (error) {
@@ -9,14 +20,19 @@ req.get(movies, (error, data) => {
   } else {
     characters = JSON.parse(data.body).characters;
     for (i in characters) {
-      req.get(characters[i], (error, data) => {
-        if (error) {
-          console.log(error);
-        } else {
-          person = JSON.parse(data.body).name;
-          console.log(person);
-        }
+      makeGetRequest(characters[i]).then((result) => {
+        console.log(JSON.parse(result).name);
       });
     }
   }
 });
+/*
+    characters = JSON.parse(data.body).characters;
+    for (i in characters) {
+      fetch(characters[i]).then((data) => {
+          person = JSON.parse(data.body).name;
+          name.push("person");
+        });
+      };
+    }
+}); */
